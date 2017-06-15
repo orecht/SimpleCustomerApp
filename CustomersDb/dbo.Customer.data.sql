3735c11a5aa254045f9513fecfe64d7287e45b5f
Oren Recht <orecht.lists@gmail.com>
2017-06-16 00:55:42 +0100

    Add test data for database

---
+++ CustomersDb/dbo.Customer.data.sql
@@ -0,0 +1,6 @@
+SET IDENTITY_INSERT [dbo].[Customer] ON
+INSERT INTO [dbo].[Customer] ([Id], [CustomerName], [CustomerAddress], [JoiningDate]) VALUES (2, N'Oren', N'72 Quai de la loire', N'2017-06-19 00:00:00')
+INSERT INTO [dbo].[Customer] ([Id], [CustomerName], [CustomerAddress], [JoiningDate]) VALUES (3, N'Praful', N'Summit Avenue', N'2015-01-01 00:00:00')
+INSERT INTO [dbo].[Customer] ([Id], [CustomerName], [CustomerAddress], [JoiningDate]) VALUES (4, N'Joe', N'Green Lane', N'2016-02-15 00:00:00')
+INSERT INTO [dbo].[Customer] ([Id], [CustomerName], [CustomerAddress], [JoiningDate]) VALUES (5, N'Jane', N'Green Lane', N'2017-02-14 00:00:00')
+SET IDENTITY_INSERT [dbo].[Customer] OFF
