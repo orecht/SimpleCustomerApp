6a60e3002469bdf7fa99dee9153c042d4fbc760d
Oren Recht <orecht.lists@gmail.com>
2017-07-25 18:30:37 +0100

    Cleanup: remove jQuery remove unused views and controller methods

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 using Movolytics.Models;
-using Microsoft.Extensions.Logging;
 
 namespace Movolytics.Controllers
 {
@@ -13,20 +8,13 @@ namespace Movolytics.Controllers
     {
         private IDataRepository _repository;
 
-        public HomeController(IDataRepository repository, ILogger<HomeController> logger)
+        public HomeController()
         {
-            _repository = repository;
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult GetCustomersJoiningAfter15022016()
-        {
-            var model = _repository.GetCustomersJoiningAfter(new DateTime(2016, 02, 15));
-            return View(model);
         }
 
         public IActionResult Error()
