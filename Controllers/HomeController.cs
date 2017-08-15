920088311197b4b418ed963bf0f6c2237e8767a5
Oren Recht <orecht.lists@gmail.com>
2017-08-15 12:46:31 +0100

    Remove unused property

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,8 +5,7 @@ using SimpleCustomerApp.Models;
 namespace SimpleCustomerApp.Controllers
 {
     public class HomeController : Controller
-    {
-        private IDataRepository _repository;
+    { 
 
         public HomeController()
         {
